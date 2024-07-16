e738376a9a24486f6127f318ad31af5f29ee530a
lxxx <1318753541@qq.com>
2024-07-16 17:06:13 +0800

    feat(formatDuration): 优化可显示小时数

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 /**
- * 将毫秒值转换成分:秒，最大60分钟
+ * 将毫秒值转换成  "时:分:秒"
  * @param duration 时长字符串(毫秒值)
  */
 export function formatDuration(duration: number) {
-  const minute = Math.floor(duration / 1000 / 60).toString().padStart(2, '0')
+  let minute = Math.floor(duration / 1000 / 60).toString().padStart(2, '0')
   const second = (duration / 1000 % 60).toFixed(0).toString().padStart(2, '0')
+  let hour = ''
+  if (Number(minute) >= 60) {
+    hour = Math.floor(Number(minute) / 60).toString().padStart(2, '0')
+    minute = Math.floor(Number(minute) % 60).toString().padStart(2, '0')
+  }
+  if (hour !== '') {
+    return `${hour}:${minute}:${second}`
+  }
   return `${minute}:${second}`
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,22 @@
 import { formatDuration } from '../src/index'
 
-describe('毫秒转换为分秒值', () => {
+describe('2000毫秒转换为时分秒值', () => {
+  it('format', () => {
+    const time = formatDuration(2000)
+    expect(time).toBe('00:02')
+  })
+})
+
+describe('100000毫秒转换为时分秒值', () => {
   it('format', () => {
     const time = formatDuration(100000)
     expect(time).toBe('01:40')
+  })
+})
+
+describe('3601000毫秒转换为时分秒值', () => {
+  it('format', () => {
+    const time = formatDuration(3601000)
+    expect(time).toBe('01:00:01')
   })
 })
