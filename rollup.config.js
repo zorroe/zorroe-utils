670f458d16de695028b4c0afc418df0db03550c1
lxxx <1318753541@qq.com>
2024-07-16 13:46:08 +0800

    build(json): add @rollup/plugin-json

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 
 export default {
   input: './src/index.ts', // 入口文件
@@ -22,5 +23,5 @@ export default {
       minifyInternalExports: true,
     },
   ],
-  plugins: [typescript({ tsconfig: './tsconfig.json' }), resolve(), commonjs()],
+  plugins: [typescript({ tsconfig: './tsconfig.json' }), resolve(), commonjs(), json()],
 }
