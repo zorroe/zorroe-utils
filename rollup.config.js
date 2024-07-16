0d3151a4035804d2c53e435b37040f8974d15528
lxxx <1318753541@qq.com>
2024-07-16 10:01:22 +0800

    add formatTimestamp, formatDuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,25 @@
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+
 export default {
   input: './src/index.ts', // 入口文件
   output: [
     {
       format: 'cjs', // 打包为commonjs格式
       file: 'dist/zorroe-utils.cjs.js', // 打包后的文件路径名称
-      name: 'zutils' // 打包后的默认导出文件名称
+      name: 'zutils', // 打包后的默认导出文件名称
     },
     {
       format: 'esm', // 打包为esm格式
       file: 'dist/zorroe-utils.esm.js',
-      name: 'zutils'
+      name: 'zutils',
     },
     {
       format: 'umd', // 打包为umd通用格式
       file: 'dist/zorroe-utils.umd.js',
       name: 'zutils',
-      minifyInternalExports: true
-    }
+      minifyInternalExports: true,
+    },
   ],
-  plugins: [typescript({ tsconfig: './tsconfig.json' }), resolve()]
+  plugins: [typescript({ tsconfig: './tsconfig.json' }), resolve()],
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+import dayjs from 'dayjs'
+
 export function random(min: number, max: number): number {
-    return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+export function formatTimeStamp(timestamp: string | number, format = 'YYYY-MM-DD') {
+  return dayjs(timestamp).format(format)
+}
+
+export function formatDuration(duration: number) {
+  const minute = Math.floor(duration / 1000 / 60).toString().padStart(2, '0')
+  const second = (duration / 1000 % 60).toFixed(0).toString().padStart(2, '0')
+  return `${minute}:${second}`
 }
